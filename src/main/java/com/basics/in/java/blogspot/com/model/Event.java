ead299a57ceb06820793d0008dcedcb57be2baf8
sudeep cv <deepam23590@gmail.com>
2015-05-21 21:03:06 +0530

    springmvc4 skeleton

---
+++ src/main/java/com/basics/in/java/blogspot/com/model/Event.java
@@ -0,0 +1,16 @@
+package com.basics.in.java.blogspot.com.model;
+
+public class Event {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ src/main/java/com/basics/in/java/blogspot/com/view/PhoneConstraintValidator.java
@@ -0,0 +1,21 @@
+package com.basics.in.java.blogspot.com.view;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PhoneConstraintValidator implements ConstraintValidator<Phone, String> {
+
+	@Override
+	public void initialize(Phone phone) {
+		
+	}
+	
+	@Override
+	public boolean isValid(String phoneField, ConstraintValidatorContext cxt) {
+		if(phoneField == null) {
+			return false;
+		}
+		
+		return phoneField.matches("[0-9()-]*");
+	}
+}
